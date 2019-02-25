d95bb454d12d26c04d55b2596998de4ba3154304
Pierre Cariou <pierrecariou@outlook.fr>
2019-02-25 15:34:23 +0100

    conroller

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,27 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @article = Article.new
+  end
+
+  def create
+    @article = article.new(article_params)
+    @article.user = User.find(params[:user_id])
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def article_params
+    params.require(:user).permit(:category, :subcategory, :URL, :description, :upvotes, :fake_news)
+  end
+
+end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependant: :destroy
 end
