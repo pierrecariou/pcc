b26b90111470819dec036b6ebbbf1e5db1051656
Theodore Brossollet <theodore.brossollet@edu.emlyon.com>
2019-02-26 15:36:47 +0100

    attention ca va péter, frontshowarticle et comment

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles do
-    resources :comments
+    resources :comments, only: [:new, :create]
   end
 end
