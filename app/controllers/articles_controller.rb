cfb95c3e77477965c1f7696d24c0a5f08d7416c4
Matthieu Montagne <matthieumontagne@gmail.com>
2019-02-28 16:30:54 +0100

    scrap des URL : OK + modification de la card

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.user = current_user
-    raise
+    scrap(@article.URL)
 
     if @article.save
       redirect_to articles_path
@@ -37,4 +37,15 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:URL, :description, :category_id)
   end
 
+  def scrap(url)
+    html_file = open(url).read
+    html_doc = Nokogiri::HTML(html_file)
+    @article.title = html_doc.css('html > head > title').text
+    @article.site_description = html_doc.xpath('/html/head/meta[@name="description"]/@content').to_s
+    head_image = html_doc.xpath('/html/head/meta[@property="og:image"]/@content').to_s
+    first_body_image = html_doc.search('img').first.attribute('src').value if !html_doc.search('img').first.attribute('src').nil?
+    image = head_image.empty? ? first_body_image : head_image
+    @article.image = image
+    @article.source = URI.parse(url).host
+  end
 end
