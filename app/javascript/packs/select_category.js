803094b9a63f7b302407948bc748ef021dd5e498
Valentine Giraudat <valentine.giraudat@gmail.com>
2019-03-06 18:27:39 +0100

    selection catégories et sous-catégories dans le create

--- app/javascript/packs/select_category.js
+++ app/javascript/packs/select_category.js
@@ -2,22 +2,39 @@ const categories = document.querySelectorAll('.category-image')
 const categoryBoxes = document.querySelectorAll('.sub-categories')
 const subcategories = document.querySelectorAll('.subcategory-choice');
 
+
+function unselectSubCategories(option) {
+  const activeLabels = option.querySelectorAll('label.active');
+  activeLabels.forEach((label) => {
+    const forAttr = label.getAttribute("for");
+    const relatedInput = option.querySelector(`#${forAttr}`);
+    relatedInput.click();
+  })
+}
+
+function activateOnlyClickedCategory(event) {
+  const categoryOptions = Array.from(document.querySelectorAll('.category-box'));
+  const currentCategoryOption = event.currentTarget.parentElement;
+  const currentCategoryOptionIndex = categoryOptions.indexOf(currentCategoryOption);
+  const unselectedCategoryOptions = categoryOptions.filter((option, index) => index !== currentCategoryOptionIndex);
+  if (currentCategoryOption.classList.contains('not-active')) {
+    currentCategoryOption.classList.remove('not-active');
+  }
+  unselectedCategoryOptions.forEach((option) => {
+    if (!option.classList.contains('not-active')) {
+      option.classList.add('not-active');
+    }
+
+    unselectSubCategories(option);
+  });
+}
+
 const selectCategory = () => {
 
   categories.forEach((category) => {
     category.addEventListener('click', (event) => {
-      const categoryOptions = Array.from(document.querySelectorAll('.category-box'));
-      const currentCategoryOption = event.currentTarget.parentElement;
-      const currentCategoryOptionIndex = categoryOptions.indexOf(currentCategoryOption);
-      const unselectedCategoryOptions = categoryOptions.filter((option, index) => index !== currentCategoryOptionIndex);
-      if (currentCategoryOption.classList.contains('not-active')) {
-        currentCategoryOption.classList.remove('not-active');
-      }
-      unselectedCategoryOptions.forEach((option) => {
-        if (!option.classList.contains('not-active')) {
-          option.classList.add('not-active');
-        }
-      });
+
+      activateOnlyClickedCategory(event);
 
       subcategories.forEach((subcategory) => {
         subcategory.classList.remove('active');
