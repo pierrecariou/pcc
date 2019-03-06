c457878de1239ebcc0da6af72177fa6e42287f99
Valentine Giraudat <valentine.giraudat@gmail.com>
2019-03-06 17:33:29 +0100

    ok

--- app/javascript/packs/select_category.js
+++ app/javascript/packs/select_category.js
@@ -4,16 +4,29 @@ const subcategories = document.querySelectorAll('.subcategory-choice');
 
 const selectCategory = () => {
 
-  categories.forEach( (category) => {
+  categories.forEach((category) => {
     category.addEventListener('click', (event) => {
-      subcategories.forEach( (subcategory) => {
+      const categoryOptions = Array.from(document.querySelectorAll('.category-box'));
+      const currentCategoryOption = event.currentTarget.parentElement;
+      const currentCategoryOptionIndex = categoryOptions.indexOf(currentCategoryOption);
+      const unselectedCategoryOptions = categoryOptions.filter((option, index) => index !== currentCategoryOptionIndex);
+      if (currentCategoryOption.classList.contains('not-active')) {
+        currentCategoryOption.classList.remove('not-active');
+      }
+      unselectedCategoryOptions.forEach((option) => {
+        if (!option.classList.contains('not-active')) {
+          option.classList.add('not-active');
+        }
+      });
+
+      subcategories.forEach((subcategory) => {
         subcategory.classList.remove('active');
       })
       const categoryId = event.currentTarget.dataset.categoryId;
       const subCategoryBox = document.getElementById(categoryId);
 
       categoryBoxes.forEach(box => box.classList.add('hidden'));
-      subCategoryBox.classList.toggle('hidden');
+        subCategoryBox.classList.toggle('hidden');
     })
   })
 }
