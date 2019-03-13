0681faefa1d2885efc0c1341cbef96efbc6861e4
Pierre Cariou <pierrecariou@outlook.fr>
2019-03-13 17:13:52 +0100

    debat

---
+++ app/javascript/packs/debat.js
@@ -0,0 +1,33 @@
+const debatTitles = document.querySelectorAll('.title-comment');
+const debats = document.querySelectorAll('.user-comment-card');
+
+
+function hideOtherDebates() {
+  const debatTitle = document.querySelector('.title-comment');
+  debatTitle.classList.add('active');
+  debats.forEach((debat) => {
+     if (debat.querySelector('.title-comment').innerText.trim() != debatTitle.innerText.trim()) {
+          debat.style.display = "none";
+        }
+});
+
+
+  debatTitles.forEach((title) => {
+    title.addEventListener('click', (event) => {
+      debatTitles.forEach((title) => {
+        title.classList.remove('active');
+      });
+      title.classList.add('active');
+      debats.forEach((debat) => {
+        debat.style.display = "none";
+        const titleInsideDebat = debat.querySelector('.title-comment')
+       if (titleInsideDebat.innerText.trim() === event.currentTarget.innerText.trim()) {
+          debat.style.display = "block";
+        }
+      });
+
+    });
+  });
+};
+
+export { hideOtherDebates };
