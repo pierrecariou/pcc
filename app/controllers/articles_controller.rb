29ab811c1655b27c2f5df49e527b523828d4ffb0
Pierre Cariou <pierrecariou@outlook.fr>
2019-03-27 17:24:50 +0100

    yes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -62,7 +62,7 @@ class ArticlesController < ApplicationController
     @article.category = cat
 
     if @article.save
-      redirect_to articles_path(query: { category_name: @article.category.name }, anchor: 'new-article-anchor')
+      redirect_to articles_path(query: { category_name: @article.category.name, date_from: -1.days.from_now }, anchor: 'new-article-anchor')
     else
       render :new
     end
