0e0c6005e26e6100a17fe2ba7a5e6f78f70ac761
Pierre Cariou <pierrecariou@outlook.fr>
2019-04-03 17:27:31 +0200

    comeback

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: 'articles#index', date_from: -7.days.from_now
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles, only: [ :index, :show, :create ] do
-    resources :comments, only: [ :new, :create ] do
+    resources :comments, only: [ :new, :create, :index ] do
       resources :sub_comments, only: [ :new, :create ]
     end
   end
