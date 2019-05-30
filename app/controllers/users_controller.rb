ae6d66a37f9944025cde3dedb854d6ff3f7b188d
Pierre Cariou <pierrecariou@outlook.fr>
2019-05-30 16:00:22 +0200

    yes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     authorize @user
-    if current_user == @user
-       @user.photo_url = 'logopcc.png'
-    end
     @comment_article = CommentArticle.new
   end
 
