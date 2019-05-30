efc1fa8419b9116f0c531be447d9451e05262011
Pierre Cariou <pierrecariou@outlook.fr>
2019-05-30 15:17:12 +0200

    photourl

--- db/migrate/20190305101721_add_photo_url_to_articles.rb
+++ db/migrate/20190305101721_add_photo_url_to_articles.rb
@@ -1,5 +1,1 @@
-class AddPhotoUrlToArticles < ActiveRecord::Migration[5.2]
-  def change
-    add_column :articles, :photo_url, :string
-  end
-end
+20190305101721_add_photo_url_to_articles.rb
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_22_135155) do
+ActiveRecord::Schema.define(version: 2019_05_30_131202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -162,7 +162,7 @@ ActiveRecord::Schema.define(version: 2019_05_22_135155) do
     t.integer "age"
     t.string "activity"
     t.string "photo"
-    t.string "photo_url"
+    t.string "photo_url", default: "logopcc.png"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
