84bb567263090824b2e5a5f0623f720213111d01
pierrecariou <pierrecariou@outlook.fr>
2021-06-16 16:51:57 +0200

    yarn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit
   before_action :set_categories
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protect_from_forgery with: :exception
 
-#	def configure_permitted_parameters
-#		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation,:first_name) }
-#	end
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :first_name, :last_name, :password, :password_confirmation) }
+	end
 
   #include Pundit
 
